b724fc4131a5b630c77b2caafb7a4ff1c50fd9c3
Suchit Deshmukh <Suchitd1008@gmail.com>
2026-06-29 09:45:41 +0000

    Initial commit

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template
+import socket
+from datetime import datetime
+import platform
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template(
+        "index.html",
+        hostname=socket.gethostname(),
+        current_time=datetime.now().strftime("%d %B %Y | %I:%M:%S %p"),
+        os_name=platform.system(),
+        python_version=platform.python_version(),
+        version="v1.0.0",
+        environment="Production",
+        status="LIVE"
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
